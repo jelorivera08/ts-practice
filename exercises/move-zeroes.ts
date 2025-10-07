5a94fa4b9a26f0413f69db5369a4f5c03a8b4d4c
jelorivera08 <jelorivera08@gmail.com>
2025-10-07 21:34:16 +1100

    better move zeroes

--- exercises/move-zeroes.ts
+++ exercises/move-zeroes.ts
@@ -2,12 +2,29 @@
  Do not return anything, modify nums in-place instead.
  */
 function moveZeroes(nums: number[]): void {
-  const nonzero = nums.filter((v) => v !== 0);
-  const zero = nums.filter((v) => v === 0);
-  const correct = [...nonzero, ...zero];
+  let pointer = 0;
+  let sorting = true;
+  const totalZeroLength = nums.length - nums.filter((v) => v === 0).length;
 
-  for (let [i, c] of correct.entries()) {
-    nums[i] = c;
+  while (sorting) {
+    // real
+    if (nums[pointer] === 0) {
+      for (let i = pointer; i < nums.length - 1; i++) {
+        nums[i] = nums[i + 1];
+      }
+
+      nums.pop();
+      nums.push(0);
+    }
+
+    if (pointer < totalZeroLength && nums[pointer] === 0) {
+      pointer = 0;
+    } else if (pointer >= nums.length - 1 || pointer === totalZeroLength) {
+      sorting = false;
+    } else if (pointer < nums.length - 1) {
+      pointer++;
+    }
   }
+
   console.log(nums);
 }
